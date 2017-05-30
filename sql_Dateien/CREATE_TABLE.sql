a26c29ef65b0dd5330405baa8ddc1921ed4df045
sekores <serkan.baris.94@googlemail.com>
2017-05-30 16:45:17 +0200

    Datenbankschema Implementierung

---
+++ sql_Dateien/CREATE_TABLE.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE IF NOT EXISTS Tweet(
+	ID SERIAL,
+	anzahl_geteilt int,
+	anzahl_like int,
+	text VARCHAR,
+	Datum date,
+	Benutzer VARCHAR,
+	Uhrzeit time,
+	CONSTRAINT tweet_pkey PRIMARY KEY (ID)
+	);
+
+CREATE TABLE IF NOT EXISTS Hashtag(
+	text VARCHAR,
+	CONSTRAINT hashtag_pkey PRIMARY KEY (text)
+	);
+
+CREATE TABLE IF NOT EXISTS Tweet_Hashtag (
+	tweet_id SERIAL REFERENCES  Tweet(ID) ON UPDATE CASCADE,
+	Hashtag_text VARCHAR REFERENCES Hashtag(text) ON UPDATE CASCADE,
+	CONSTRAINT tweet_hashtag_pkey PRIMARY KEY (tweet_id, Hashtag_text)
+	);
