82652fba0b3aa10ae3d971fff8c06b0febc46a7a
sekores <serkan.baris.94@googlemail.com>
2017-05-30 17:46:02 +0200

    sql

--- sql_Dateien/CREATE_TABLE.sql
+++ sql_Dateien/CREATE_TABLE.sql
@@ -7,15 +7,15 @@
 	Benutzer VARCHAR,
 	Uhrzeit time,
 	CONSTRAINT tweet_pkey PRIMARY KEY (ID)
-	);
+	)
 
 CREATE TABLE IF NOT EXISTS Hashtag(
 	text VARCHAR,
 	CONSTRAINT hashtag_pkey PRIMARY KEY (text)
-	);
+	)
 
 CREATE TABLE IF NOT EXISTS Tweet_Hashtag (
 	tweet_id SERIAL REFERENCES  Tweet(ID) ON UPDATE CASCADE,
 	Hashtag_text VARCHAR REFERENCES Hashtag(text) ON UPDATE CASCADE,
 	CONSTRAINT tweet_hashtag_pkey PRIMARY KEY (tweet_id, Hashtag_text)
-	); 
+	)
