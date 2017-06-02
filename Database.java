295c0d44d881c3cb86c2977972d66e2680bc546e
sekores <serkan.baris.94@googlemail.com>
2017-06-02 12:08:53 +0200

    Insert Tweet & Insert Hashtag

--- Database.java
+++ Database.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
@@ -18,7 +20,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Database {
-
 	
     public static Connection getConnect(){
     	
@@ -32,7 +33,7 @@ public class Database {
         
         try {
             c = DriverManager.getConnection(url,username,password);
-            System.out.println("Verbindung zur Datenbank "+db+" mit dem Benutzer "+username+" wurde erfolgreich hergestellt");
+            //System.out.println("Verbindung zur Datenbank "+db+" mit dem Benutzer "+username+" wurde erfolgreich hergestellt");
         }catch (Exception e) {
         	e.printStackTrace();
         	System.out.println("Verbindung zur Datenbank "+db+" mit dem Benutzer "+username+" konnte nicht hergestellt werden");
@@ -41,7 +42,7 @@ public class Database {
 		return c;
     }
    
-    public static void getExcel() throws IOException{
+	public static void getExcel() throws IOException{
     	
     		String excelFilePath = "/home/serkan/Dokumente/Datenbanksysteme/Projekt/american-election-tweets.xlsx";
 			FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
@@ -49,27 +50,142 @@ public class Database {
 			Workbook workbook = new XSSFWorkbook(inputStream);
 			Sheet firstSheet = (Sheet) workbook.getSheetAt(0);
 			Iterator<Row> iterator = firstSheet.iterator();
+			iterator.next();
 				
+			
+            List<String> temp = new ArrayList<String>();
+            
+            
 				while(iterator.hasNext()){
 					Row nextRow = iterator.next();
 					Iterator<Cell> cellIterator = nextRow.cellIterator();
 					
+					temp.clear();
 					while (cellIterator.hasNext()) {
-		                Cell cell = cellIterator.next();
-		                String[] temp;//Hier waren wir stehen geblieben
-		                //Idee: Alle Cells in eine Liste von Strings einfügen und damit weiter arbeiten.
-		                System.out.println(cell.toString());
+		                Cell cell = cellIterator.next(); 
+		                
+		                temp.add(cell.toString());
+		                
 		            }
-		            System.out.println();
+					
+					if(temp.get(3).length()==19 && temp.get(3).contains("T") && temp.get(3).contains("2016")){
+						temp.add(3," ");
+					}
+					if(temp.get(5).length()==4 && temp.get(5).equals("True")|| temp.get(5).length()==5 && temp.get(5).equals("False")){
+						temp.add(5," ");
+					}
+					
+					try {
+						Connection election = getConnect();
+						
+						PreparedStatement insertTweet = election.prepareStatement("INSERT INTO  Tweet(anzahl_geteilt, anzahl_like, text, datum, benutzer, uhrzeit) VALUES ("+temp.get(7)+","+temp.get(8)+",'"+tweet(temp.get(1))+"','"+date(temp.get(4))+"','"+temp.get(0)+"','"+time(temp.get(4))+"')");
+						insertTweet.executeUpdate();
+						
+						
+						
+						election.close();
+						
+						insertHashtag(temp.get(1));
+						
+			    	} catch (Exception e) {
+			    		//e.printStackTrace();
+			    		//System.out.println("INSERT INTO  Tweet(anzahl_geteilt, anzahl_like, text, datum, benutzer, uhrzeit) VALUES ("+temp.get(7)+","+temp.get(8)+",'"+tweet(temp.get(1))+"','"+date(temp.get(4))+"','"+temp.get(0)+"','"+time(temp.get(4))+"')");
+					}
+		            
 					workbook.close();
 					inputStream.close();
 				}
+				
     	
     	
         
     }
     
-    public static void getCSV() {
+	public static void insertHashtag(String input) {
+		
+		while(input.contains("#")){
+			String result="";
+			int hashtag_index=input.indexOf("#")+1;
+			while(input.charAt(hashtag_index)!=' '){
+					
+				//Solange an der (Stelle #)+1 bis Space abspeichern
+				result=result+input.charAt(hashtag_index);
+				hashtag_index++;
+
+			}
+				
+			input=input.substring(hashtag_index, input.length());
+				
+			System.out.println(result);
+			try {		
+				Connection conn = getConnect();
+				//In die Tabelle einfügen
+				PreparedStatement insertHashtag = conn.prepareStatement( "INSERT INTO Hashtag (text) VALUES ('"+result+"') ");
+				insertHashtag.executeUpdate();
+				conn.close();
+				
+			} catch (Exception e) {
+					
+				e.printStackTrace();
+				System.out.println("INSERT INTO Hashtag(text) VALUES ('"+result+"');");
+				
+			}
+		}
+	}
+
+	public static String tweet(String input) {
+		String result="";
+		
+		if(input.contains("\"") && input.contains("'")){
+			result=input.replace("\"", "");
+			result=input.replace("'", "");
+			
+			return result;
+		}else
+		
+		if (input.contains("\"")) {
+			result=input.replace("\"", "");
+
+			return result;
+		}else
+		
+		if(input.contains("'")){
+			result=input.replace("'", "");
+
+			return result;
+			
+		}else{
+			return input;
+		}
+		
+	}
+
+	
+	public static String date(String input) {
+		String result="";
+		int i = 0;
+		while(input.charAt(i)!='T'){
+			result=result+input.charAt(i);
+			i++;
+		}
+		return result;
+	}
+	
+	
+	public static String time(String input) {
+		String result="";
+		int i = input.indexOf('T') + 1;
+		
+		while(i != input.length()){
+			result=result+input.charAt(i);
+			i++;
+		}
+	
+		return result;
+	}
+
+	//ursprünglich wollten wir es mit der CSV machen,  jedoch ahben wir uns dann auf excel umentschieden
+	public static void getCSV() {
     	 	FileReader fr;
 			try {
 				fr = new FileReader("/home/serkan/Dokumente/Datenbanksysteme/Projekt/american-election-tweets.csv");
@@ -94,8 +210,9 @@ public class Database {
     	    
     }
   
-    public static void main(String[] args) {
-       
+    
+	public static void main(String[] args) {
+    	
     	
     	try {
 			getExcel();
